b63f5c966deb26560615fc5e92ae62525046f6db
Rama Praveen Pyla <ramapraveen@fb.com>
2018-07-06 23:53:50 -0700

    update simple_throw_catch

--- exception/simple_throw_catch.cpp
+++ exception/simple_throw_catch.cpp
@@ -1,16 +1,40 @@
 
 #include <iostream>
 
+using namespace std;
+
 class MyError {
 	const char *data;
 public:
 	MyError(const char *msg) : data(msg) {}
 };
 
+class Rainbow {
+public:
+	Rainbow() { std::cout<<"In Raibow constructor"<<endl; }
+	~Rainbow() { std::cout<<"In Raibow destructor"<<endl; }
+};
+
 void f() {
+	Rainbow rb;
 	throw MyError("something bad happened");
 }
 
 int main() {
-	f();
+	try {
+		f();
+	} catch (MyError err) {
+		cout << "exception caught"<<endl;
+	}
+	cout<<"after try catch block"<<endl;
+	return 0;
 }
+
+/*
+	Output:
+
+In Raibow constructor
+In Raibow destructor
+exception caught
+after try catch block
+*/
